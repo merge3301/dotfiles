9e344878049324677333f838db0e3f8e8a499656
merge3301 <artyom.naumkin@bk.ru>
2026-06-13 16:12:25 +0300

    add toggleterm keymaps

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -16,6 +16,9 @@ map("n", "<C-j>", "<C-w>j", { desc = "Move to bottom split" })
 map("n", "<C-k>", "<C-w>k", { desc = "Move to top split" })
 map("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
 
+map("n", "|", ":vsplit<CR>")
+map("n", "\\", ":split<CR>")
+
 local builtin = require("telescope.builtin")
 map("n", "<leader>f", builtin.find_files, { desc = "Find files (root dir)" })
 map("n", "<leader>g", builtin.live_grep, { desc = "Live grep (root dir)" })
@@ -24,6 +27,11 @@ map("n", "<leader>b", builtin.buffers, { desc = "Find buffers" })
 map("n", "<Tab>", ":bnext<CR>", { desc = "Next buffer" })
 map("n", "<S-Tab>", ":bprevious<CR>", { desc = "Previous buffer" })
 map("n", "<Leader>d", ":bdelete<CR>", { desc = "Close buffer" })
+
+map("t", "<C-h>", [[<Cmd>wincmd h<CR>]], { desc = "Move to left split" })
+map("t", "<C-j>", [[<Cmd>wincmd j<CR>]], { desc = "Move to bottom split" })
+map("t", "<C-k>", [[<Cmd>wincmd k<CR>]], { desc = "Move to bottom split" })
+map("t", "<C-l>", [[<Cmd>wincmd l<CR>]], { desc = "Move to bottom split" })
 
 map("t", "<Esc>", [[<C-\><C-n>]], { desc = "Normal mode" })
 map("t", "nn", [[<C-\><C-n>]], { desc = "Normal mode" })
